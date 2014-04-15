dfc59c8a989213498174ac9671a61a483797c26b
system_user <tyler.elric@gmail.com>
2014-04-15 11:10:58 -0500

    Frame counting / skipping working appropriately. to-do: instantiate run-time objects.

--- pypk.py
+++ pypk.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python
+
 import json
 
-split_species = lambda s,n:"".join(t.strip() for t in s.split(n))
+split_pieces = lambda s,n:iter(t.strip() for t in s.split(n))
 
 class MetaEvent:
 	def __init__(self):self.handlers=[]
@@ -28,26 +30,64 @@ class Deferred:
 class SceneNode:
 	def __init__(self,name,graphnode):
 		self.name = name
-		self.sub_nodes = dict((name,SceneNode(name,node)) for name,node in graphnode['sub_nodes'])
+		subs = graphnode.get('sub_nodes',{})
+		self.sub_nodes = dict((name,SceneNode(name,node)) for name,node in subs.items())
 		runtime_type = graphnode['type']
-		runtime_args, runtime_kwargs = graphnode['args'],graphnode['kwargs']
+		runtime_args, runtime_kwargs = graphnode.get('args',[]),graphnode.get('kwargs',{})
 		self.runtime_object = SceneNode.make_object(runtime_type,*runtime_args,**runtime_kwargs)
 	@classmethod
 	def make_object(cls,typename,*args,**kwargs):
-		typename = split_pieces(typename,".")
+		typename = list(split_pieces(typename,"."))
 		module,instance = typename[:-1],typename[-1]
 		#instance = load_module(module)[instance]
 		#instance = instance(*args,**kwargs)
 		return instance
+	def tick(self,scene_manager,scene_scope,*a,**k):
+		print(self.runtime_object,self.name)
 
 class SceneScope:
-	def __init__(self,root_scene):
-		root_scene = root_scene if isinstance(root_scene,SceneNode) else SceneNode(root_scene)
+	def __init__(self,root_scene,scope=None):
 		self.root_scene = root_scene
-		self.scope_frequency = {}
+		self.scope_frequency = scope or {}
 		self.tick_step = 0
+	def tick(self,manager,*a,**k):
+		self.tick_step += 1
+		for path,ticks in self.scope_frequency.items():
+			if ticks==1 or self.tick_step%ticks==0:#ran every tick
+				self.get_node(path).tick(manager,self,*a,**k)
+	def get_node(self,path,root_node=None):
+		root_node = root_node or self.root_scene
+		path = list(split_pieces(path,".")) if isinstance(path,str) else path
+		if len(path)>1:
+			return self.get_node(path[1:],root_node.sub_nodes[path[0]])
+		return root_node.sub_nodes[path[0]]
 
 class SceneManager:
 	def __init__(self,initial_scene,initial_scope):
 		self.initial_scene = self.scene = initial_scene
 		self.initial_scope = self.scope = initial_scope
+	def tick(self,*a,**k):
+		self.scope.tick(self,*a,**k)
+
+if __name__=="__main__":
+	root_node = SceneNode("title_screen",{
+		"type":"pypk.TitleScreen",
+		"sub_nodes":{
+			"text_flasher":{
+				"type":"pypk.TextFlasher",
+				"args":["Press `Enter` to continue!"],
+				"sub_nodes":{
+					"exit_titlescreen":{
+						"type":"pypk.ContinueOnEnter",
+					}
+				}
+			}
+		}
+	})
+	root_scope = SceneScope(root_node,{
+		"text_flasher":4,
+		"text_flasher.exit_titlescreen":1
+	})
+	manager = SceneManager(root_node,root_scope)
+	while True:
+		manager.tick()
